46c98daacaba4ff8215f7b6984a93ae6994a6c25
saskaradit <saskaradit@gmail.com>
2021-11-22 20:44:06 +0700

    dependency inversion

--- SOLID/dependency-inversion/main.go
+++ SOLID/dependency-inversion/main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import "fmt"
 
+// Dependency Inversion Principle
+// HLM should not depend on LLM
+// Both should depend on abstractions
+
+type Relationship int
+
+const (
+	Parent Relationship = iota
+	Child
+	Sibling
+)
+
+type Person struct {
+	name string
+	//
+}
+
+type Info struct {
+	from         *Person
+	relationship Relationship
+	to           *Person
+}
+
+// Low-level module
+type RelationshipBrowser interface {
+	FindAllChildrenOf(name string) []*Person
+}
+
+type Relationships struct {
+	relations []Info
+}
+
+func (r *Relationships) FindAllChildrenOf(name string) []*Person {
+	result := make([]*Person, 0)
+
+	for i, v := range r.relations {
+		if v.relationship == Parent && v.from.name == name {
+			result = append(result, r.relations[i].to)
+		}
+	}
+	return result
+}
+
+func (r *Relationships) AddParentAndChild(parent, child *Person) {
+	r.relations = append(r.relations, Info{parent, Parent, child})
+	r.relations = append(r.relations, Info{child, Child, parent})
+}
+
+// High-level module
+type Research struct {
+	// break DIP
+	// relationships Relationships
+	browser RelationshipBrowser
+}
+
+func (r *Research) Investigate() {
+	for _, p := range r.browser.FindAllChildrenOf("John") {
+		fmt.Println("John has a child called", p.name)
+	}
+}
+
+func main() {
+	parent := Person{"John"}
+	child1 := Person{"Rad"}
+	child2 := Person{"Sas"}
+
+	relationships := Relationships{}
+	relationships.AddParentAndChild(&parent, &child1)
+	relationships.AddParentAndChild(&parent, &child2)
+
+	r := Research{&relationships}
+	r.Investigate()
 }
