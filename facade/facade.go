ab541670d3ab4e4b3b0f87cdb8d9883688e6e70a
saskaradit <saskaradit@gmail.com>
2021-11-24 20:56:31 +0700

    facade

---
+++ facade/facade.go
@@ -0,0 +1,47 @@
+package main
+
+type Buffer struct {
+	width, height int
+	buffer        []rune
+}
+
+func NewBuffer(width int, height int) *Buffer {
+	return &Buffer{width, height, make([]rune, width*height)}
+}
+
+func (b *Buffer) At(index int) rune {
+	return b.buffer[index]
+}
+
+type Viewport struct {
+	buffer *Buffer
+	offset int
+}
+
+func NewViewport(buffer *Buffer) *Viewport {
+	return &Viewport{}
+}
+
+func (v *Viewport) GetCharacterAt(index int) rune {
+	return v.buffer.At(v.offset + index)
+}
+
+type Console struct {
+	buffer    []*Buffer
+	viewports []*Viewport
+	offset    int
+}
+
+func NewConsole() *Console {
+	b := NewBuffer(200, 150)
+	v := NewViewport(b)
+	return &Console{[]*Buffer{b}, []*Viewport{v}, 0}
+}
+
+func (c *Console) GetCharacterAt(index int) rune {
+	return c.viewports[0].GetCharacterAt(index)
+}
+
+func main() {
+	_ = NewConsole()
+}
