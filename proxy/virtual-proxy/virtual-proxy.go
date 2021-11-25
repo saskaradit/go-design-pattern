5ea8d00ec9d207abaaf3629280b2ff70c619e410
saskaradit <saskaradit@gmail.com>
2021-11-25 13:26:07 +0700

    virtual proxy

---
+++ proxy/virtual-proxy/virtual-proxy.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type Image interface {
+	Draw()
+}
+
+type Bitmap struct {
+	filename string
+}
+
+func (b *Bitmap) Draw() {
+	fmt.Println("Drawing image", b.filename)
+}
+
+func NewBitmap(filename string) *Bitmap {
+	fmt.Println("Loading image from", filename)
+	return &Bitmap{filename}
+}
+
+func DrawImage(image Image) {
+	fmt.Println("About to draw the image")
+	image.Draw()
+	fmt.Println("Done drawing the image")
+}
+
+type LazyBitmap struct {
+	filename string
+	bitmap   *Bitmap
+}
+
+func (l *LazyBitmap) Draw() {
+	if l.bitmap == nil {
+		l.bitmap = NewBitmap(l.filename)
+	}
+	l.bitmap.Draw()
+}
+
+func NewLazyBitmap(filename string) *LazyBitmap {
+	return &LazyBitmap{filename: filename}
+}
+
+func main() {
+	// it still constructs, even before used
+	_ = NewBitmap("demo.png")
+
+	fmt.Println("===== divider =====")
+
+	// it does not construct the bitmap until used
+	lbmp := NewLazyBitmap("lazy.png")
+	DrawImage(lbmp)
+}
